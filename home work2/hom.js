3fa6f9fbecea23d26388f398874b24f9f594e3a4
Mr.Rai <97722260+SumeetRai@users.noreply.github.com>
2022-03-07 15:06:15 +0500

    Add files via upload

---
+++ home work2/hom.js
@@ -0,0 +1,28 @@
+var prop=prompt("Enter List Letterb")
+var empty=[]
+var alphabat =["apple","book","carrot","doding"]
+for(i=0;i<alphabat.length;i++ ){
+    if(prop===alphabat[i]){
+  
+
+      empty.push(alphabat.j) 
+
+
+    }
+}
+
+console.log(empty)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
